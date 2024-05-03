698191f2fb03430d49a8cd3213a7b0e8d340e7b9
dcrnek1 <dario.crnek@gmail.com>
2024-05-03 22:15:32 +0200

    Initialized webpack configuration with hot reload

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), 
+    clean: true,
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      template: './src/index.html', 
+      title: 'Restaurant page',
+    }),
+  ],
+};
